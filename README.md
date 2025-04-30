b30cb9f6a94a5969d204574eaaac26c4bc51fdc5
mcsoud <fmcsoud@gmail.com>
2025-04-30 19:03:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A CLI tool to clone the GitHub template repositories.
+
+Simply run `npx @mcsoud/templates` and select the template you want to clone.
---
+++ bin/index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import git from "simple-git";
+import path from "path";
+import fs from "fs";
+
+const questions = [
+  {
+    name: "directoryName",
+    message: "Enter directory name for your project:",
+    type: "input",
+    default: function (answers) {
+      return answers.template;
+    },
+    validate: function (input) {
+      if (input.trim() === "") {
+        return "Directory name cannot be empty";
+      }
+      if (fs.existsSync(input)) {
+        return `Directory '${input}' already exists. Please choose another name.`;
+      }
+      return true;
+    },
+  },
+  {
+    name: "template",
+    message: "Select a template:",
+    type: "list",
+    choices: ["vite-react", "vite-svelte", "nextjs"],
+  },
+];
+
+inquirer.prompt(questions).then(async (answers) => {
+  switch (answers.template) {
+    case "nextjs":
+      console.log("Option not implemented yet");
+      break;
+    default:
+      cloneRepo(`${answers.template}-template`, answers.directoryName);
+  }
+});
+
+async function cloneRepo(tmp, targetDir) {
+  const REPO_URL = `https://github.com/McSoud/${tmp}.git`;
+  try {
+    if (fs.existsSync(targetDir)) {
+      console.log(chalk.red(`Error: Directory '${targetDir}' already exists!`));
+      return;
+    }
+
+    console.log(chalk.yellow(`Cloning repository into ${targetDir}...`));
+
+    await git().clone(REPO_URL, targetDir);
+
+    const gitDir = path.join(targetDir, ".git");
+    if (fs.existsSync(gitDir)) {
+      fs.rmSync(gitDir, { recursive: true, force: true });
+    }
+
+    console.log(
+      chalk.green(`✓ Repository successfully cloned into '${targetDir}'!`)
+    );
+  } catch (error) {
+    console.log(chalk.red("Error cloning repository:"), error.message);
+  }
+}
